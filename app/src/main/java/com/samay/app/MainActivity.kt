89a65f3129229e53bc12dd6e2d2f61212f6f73b8
aliciabelaunde <mbelaunde@ucb.edu.bo>
2026-09-18 16:54:52 -0400

    fix: import correcto de Modifier.

--- app/src/main/java/com/samay/app/MainActivity.kt
+++ app/src/main/java/com/samay/app/MainActivity.kt
@@ -22,7 +22,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.Modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
